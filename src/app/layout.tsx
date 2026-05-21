fc7fe62a36115f87ee4c326481512372cdb5f1fe
zishine <wzishine@gmail.com>
2026-05-21 11:56:19 -0400

    chore: change zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ zoom: 0.85 }}>
       <body className={`${inter.className} min-h-[100dvh] w-screen flex flex-col overflow-x-hidden`}>
         <Header />
         <main className="flex-1 w-full max-w-7xl mx-auto flex flex-col relative pb-10">
